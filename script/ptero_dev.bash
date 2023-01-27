f3fe43bbfeac3c38672c202eac5776648e8633f5
JewishLewish <lanbrokhman@gmail.com>
2023-01-26 19:56:48 -0800

    Ptero_Dev bash script

---
+++ script/ptero_dev.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+# golang generic package
+if [ ! -d /mnt/server/ ]; then
+mkdir -p /mnt/server/
+fi
+cd /mnt/server
+curl -o main.go https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/main.go
+curl -o go.mod https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/go.mod
+curl -o go.sum https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/go.sum
+mkdir -p /mnt/server/sysadmin/
+cd sysadmin
+curl -o sysadmin.c https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/sysadmin/sysadmin.c
+cd /mnt/server
+go get
+go build
+#settings.json file
+if [ ! -f /mnt/server/settings.json ]; then
+curl -o settings.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/settings.json
+fi
+#database folder
+if [ ! -d /mnt/server/databases ]; then
+mkdir -p databases
+mkdir -p databases/ExampleDB
+cd databases/ExampleDB
+curl -o config.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/databases/CatoDB/config.json
+curl -o database.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/databases/CatoDB/database.json
+fi
+cd /mnt/server
