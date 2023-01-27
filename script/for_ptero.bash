92d10489cfd015fa15cb71ec0767200d2717f97c
JewishLewish <lanbrokhman@gmail.com>
2023-01-26 19:01:22 -0800

    Updated script

--- script/for_ptero.bash
+++ script/for_ptero.bash
@@ -7,12 +7,17 @@ cd /mnt/server
 curl -o main.go https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/main.go
 curl -o go.mod https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/go.mod
 curl -o go.sum https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/go.sum
-curl -o go.sum https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/go.sum
+mkdir -p /mnt/server/sysadmin/
+cd sysadmin
+curl -o sysadmin.c https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/sysadmin/sysadmin.c
+cd /mnt/server
 go get
 go build
 rm -f main.go
 rm -f go.mod
 rm -f go.sum
+rm -f sysadmin/sysadmin.c
+rm -r sysadmin
 #settings.json file
 if [ ! -f /mnt/server/settings.json ]; then
 curl -o settings.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/settings.json
