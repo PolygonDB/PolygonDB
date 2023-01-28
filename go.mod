804c5c7845262afb3733f6ed56d15105c2dd7e2b
JewishLewish <lanbrokhman@gmail.com>
2023-01-27 21:01:35 -0800

    Changed to more efficient json parser

--- go.mod
+++ go.mod
@@ -5,3 +5,9 @@ go 1.19
 require github.com/gorilla/websocket v1.5.0
 
 require github.com/Jeffail/gabs/v2 v2.6.1
+
+require (
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
--- main.go
+++ main.go
@@ -7,7 +7,6 @@ package main
 import "C"
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
@@ -21,7 +20,11 @@ import (
 	"github.com/Jeffail/gabs/v2"
 
 	"github.com/gorilla/websocket"
+
+	jsoniter "github.com/json-iterator/go"
 )
+
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 type config struct {
 	Key string `json:"key"`
