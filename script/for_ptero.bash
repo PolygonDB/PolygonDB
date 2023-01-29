38f90120b40ff8d71c6c592e8d082e73e14b805a
JewishLewish <lanbrokhman@gmail.com>
2023-01-28 20:52:16 -0800

    Working on Debian Dockerfile

--- script/for_ptero.bash
+++ script/for_ptero.bash
@@ -4,12 +4,13 @@ if [ ! -d /mnt/server/ ]; then
 mkdir -p /mnt/server/
 fi
 cd /mnt/server
-curl -o main.go https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/main.go
-curl -o go.mod https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/go.mod
-curl -o go.sum https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/go.sum
+TAR=253f6ce735de4e82a70936b55c01ebad03c922a9
+curl -o main.go https://raw.githubusercontent.com/JewishLewish/PolygonDB/$TAR/main.go
+curl -o go.mod https://raw.githubusercontent.com/JewishLewish/PolygonDB/$TAR/go.mod
+curl -o go.sum https://raw.githubusercontent.com/JewishLewish/PolygonDB/$TAR/go.sum
 mkdir -p /mnt/server/sysadmin/
 cd sysadmin
-curl -o sysadmin.c https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/sysadmin/sysadmin.c
+curl -o sysadmin.c https://raw.githubusercontent.com/JewishLewish/PolygonDB/$TAR/sysadmin/sysadmin.c
 cd /mnt/server
 go get
 go build
@@ -20,14 +21,14 @@ rm -f sysadmin/sysadmin.c
 rm -r sysadmin
 #settings.json file
 if [ ! -f /mnt/server/settings.json ]; then
-curl -o settings.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/settings.json
+curl -o settings.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/$TAR/settings.json
 fi
 #database folder
 if [ ! -d /mnt/server/databases ]; then
 mkdir -p databases
 mkdir -p databases/ExampleDB
 cd databases/ExampleDB
-curl -o config.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/databases/CatoDB/config.json
-curl -o database.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/main/databases/CatoDB/database.json
+curl -o config.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/$TAR/databases/CatoDB/config.json
+curl -o database.json https://raw.githubusercontent.com/JewishLewish/PolygonDB/$TAR/databases/CatoDB/database.json
 fi
 cd /mnt/server
