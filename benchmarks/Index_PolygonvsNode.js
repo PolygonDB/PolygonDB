3ffae42662773c9490d0e202f92cf1d43e9b0d46
JewishLewish <lanbrokhman@gmail.com>
2023-03-29 12:43:22 -0700

    Adding Benchmarking Code

---
+++ benchmarks/Index_PolygonvsNode.js
@@ -0,0 +1,56 @@
+const WebSocket = require('ws');
+const ws = new WebSocket('ws://localhost:25565/ws');
+
+function polyMethod() {
+  const data = {
+    password: 'B123',
+    dbname: 'Search_Benchmark',
+    location: 'data',
+    action: 'index',
+    value: 'gender:male'
+  };
+
+  const sendData = () => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify(data));
+    } else {
+      setTimeout(sendData, 10);
+    }
+  };
+
+  sendData();
+  
+  ws.on('message', message => {
+    const response = JSON.parse(message);
+  });
+}
+
+function nodeMethod() {
+  const { spawn } = require('child_process');
+  const pyProg = spawn('python', ['./python/benchmark.py']);
+
+  pyProg.stdout.on('data', function(data) {
+    console.log(`Python output: ${data}`);
+  });
+}
+
+function benchmark(func) {
+  const numRuns = 90;
+  let totalTime = 0;
+
+  for (let i = 0; i < numRuns; i++) {
+    const startTime = new Date().getTime();
+    func();
+    const endTime = new Date().getTime();
+    const elapsedTime = endTime - startTime;
+    totalTime += elapsedTime;
+    //console.log(`Run ${i + 1}: Function ${func.name} took ${elapsedTime} milliseconds to execute.`);
+  }
+
+  const averageTime = totalTime / numRuns;
+  console.log(`\nAverage execution time over ${numRuns} runs: ${averageTime.toFixed(6)} milliseconds`);
+  return averageTime;
+}
+
+const polyResult = benchmark(polyMethod);
+const pyResult = benchmark(nodeMethod);
---
+++ benchmarks/Index_PolygonvsPython.py
@@ -0,0 +1,61 @@
+import json
+from websocket import create_connection
+import time
+
+
+
+ws = create_connection("ws://localhost:25565/ws")
+
+
+def Poly_Method():
+    ws.send(json.dumps(
+        {
+            'password': 'B123', 
+            'dbname': 'Search_Benchmark',
+            'location' :'data',
+            'action' : 'index',
+            'value' : 'gender:male'
+        }
+    ))
+    ws_data = json.loads(ws.recv())
+
+def Python_Method():
+    ws.send(json.dumps(
+        {
+            'password': 'B123', 
+            'dbname': 'Search_Benchmark',
+            'location' :'data',
+            'action' : 'retrieve'
+        }
+    ))
+    data = json.loads(ws.recv())
+    # Create an empty list to store the male people
+    males = []
+
+    # Iterate through each person in the data
+    for index, person in enumerate(data):
+        # Check if the person's gender is male
+        if person["gender"] == "male":
+            # If so, add the person to the list of males in the desired format
+            males.append({"Index": index, "Value": person})
+
+
+def benchmark(func, *args, **kwargs):
+    total_time = 0
+    num_runs = 90
+    
+    for i in range(num_runs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        total_time += elapsed_time
+        #print(f"Run {i+1}: Function {func.__name__} took {elapsed_time:.6f} seconds to execute.")
+    
+    avg_time = total_time / num_runs
+    print(f"\nAverage execution time over {num_runs} runs: {avg_time:.6f} seconds")
+    return avg_time
+
+Poly_Result = benchmark(Poly_Method)
+
+Py_Result = benchmark(Python_Method)
