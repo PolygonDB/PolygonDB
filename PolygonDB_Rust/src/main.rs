27df28d6d0b382bf2e22982a7b9ea00d166ba2e3
JewishLewish <frostyornot@gmail.com>
2023-10-11 12:15:28 -0700

    Json Parser

--- PolygonDB_Rust/src/main.rs
+++ PolygonDB_Rust/src/main.rs
@@ -1,22 +1,32 @@
 use serde_json::{json, Value, from_str};
+use serde::{Deserialize, Serialize};
 use std::io::{self, BufRead};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Input {
+    dbname: String,
+    location: String,
+    action: String,
+    value: serde_json::Value,
+}
 
 fn main() {
     println!("Polygon v1.7 +++");
 
     let stdin = io::stdin();
     let mut scanner = stdin.lock();
-    let mut line = String::new();
-    scanner.read_line(&mut line).unwrap();
+    //let mut line = String::new();
+    //scanner.read_line(&mut line).unwrap();
 
-    let parsed_json: Result<Value, serde_json::Error> = from_str(&line);
+    let data = r#"
+    {
+        "dbname": "name_of_database", 
+        "location": "location_in_database", 
+        "action": "record", 
+        "value": 20
+    }"#;
 
-    match parsed_json {
-        Ok(json) => {
-            println!("Parsed JSON: {:#?}", json);
-        }
-        Err(e) => {
-            println!("Failed to parse JSON: {:?}", e);
-        }
-    }
+    let parsed_json: Input = serde_json::from_str(data).unwrap();
+
+    println!("{:?}", parsed_json);
 }
