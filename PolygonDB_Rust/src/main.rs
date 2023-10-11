a0fbc87b9e37f8c32562a8235273242b95531b20
JewishLewish <frostyornot@gmail.com>
2023-10-11 16:08:04 -0700

    Proper User Input

--- PolygonDB_Rust/src/main.rs
+++ PolygonDB_Rust/src/main.rs
@@ -15,18 +15,55 @@ fn main() {
 
     let stdin = io::stdin();
     let mut scanner = stdin.lock();
-    //let mut line = String::new();
-    //scanner.read_line(&mut line).unwrap();
+    let mut data = String::new();
+    scanner.read_line(&mut data).unwrap();
 
-    let data = r#"
+    /*data = r#"
     {
         "dbname": "name_of_database", 
         "location": "location_in_database", 
         "action": "record", 
         "value": 20
-    }"#;
+    }"#.to_string();*/
 
-    let parsed_json: Input = serde_json::from_str(data).unwrap();
 
-    println!("{:?}", parsed_json);
+    if is_json(&data) {
+        let parsed_json: Input = serde_json::from_str(&data).unwrap();
+        if parsed_json.action == "record" {
+            println!("record");
+        }
+        println!("{:?}", parsed_json);
+    } else {
+        let mut byte = data.split_whitespace();
+        let target: String = byte.nth(0).unwrap().to_string();
+        println!("{:?}", target);
+
+        if  target == "CREATE_DATABASE" {
+            if byte.count() <= 1 {
+                print!(
+                    "
+                    ERROR: CREATE_DATABASE _________\n
+                                           ^^^^^^^^^
+                                               ");
+                return;
+            }
+            print!("test");
+        }
+    }
+
+
+    
+}
+
+fn is_json(text: &str) -> bool {
+    let f = text.chars().nth(0).unwrap();
+    let l = text.chars().nth(text.chars().count()-1).unwrap();
+    if f == '{' && l == '}' {
+        return true;
+    }
+    return false;
+}
+
+fn poly_error(erlevel: i8){
+    return;
 }
