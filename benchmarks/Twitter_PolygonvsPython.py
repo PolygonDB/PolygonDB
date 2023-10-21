352b1bdea8774bf697f081b7ece843f48c7d5777
JewishLewish <lanbrokhman@gmail.com>
2023-10-20 21:44:37 -0700

    Added canada.json

---
+++ benchmarks/Twitter_PolygonvsPython.py
@@ -0,0 +1,25 @@
+#Python 3.11
+#Using https://github.com/JewishLewish/PolygonDB/blob/main/databases/database.json
+from websocket import create_connection
+import timeit
+import json
+from polywrapper import *
+
+poly = PolyClient(connection_url="localhost:8080", dbname="twitter")
+
+def Poly_Method():
+    x = poly.read(location="/statuses")
+    print(x)
+
+def Python_Json_Methd():
+    with open("databases/database.json", "r") as f:
+        x= json.load(f)
+
+
+def benchmark(func, num_runs=30):
+    total_time = timeit.timeit(func, number=num_runs)
+    print(f"\nAverage execution time over 90 runs: {total_time / num_runs:.6f} seconds")
+
+if __name__ == "__main__":
+    benchmark(Poly_Method)
+    benchmark(Python_Json_Methd)
